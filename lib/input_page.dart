3bc4ab7178863fd23ebebc3298444048e4c3182a
anderies <anderies@sprout.co.id>
2020-01-05 12:04:46 +0700

    Make Customize Flutter Widget

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -8,11 +9,54 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: Row(children: <Widget>[
+                Expanded(
+                  child: ReusableCard(colour: Color(0xFF1D1E33),),
+                ),
+                Expanded(
+                  child: ReusableCard(colour: Color(0xFF1D1E33),),
+                ),
+              ]),
+            ),
+            Expanded(
+              child: ReusableCard(colour: Color(0xFF1D1E33),),
+            ),
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: ReusableCard(colour: Color(0xFF1D1E33),),
+                  ),
+                  Expanded(
+                    child: ReusableCard(colour: Colors.blue),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ));
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+
+  ReusableCard({this.colour});
+
+  final Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
