a2f6b8758aa3976058c394f17e57fd376b928ab8
anderies <anderies@sprout.co.id>
2020-01-09 10:52:32 +0700

    result page

--- lib/constants.dart
+++ lib/constants.dart
@@ -9,3 +9,22 @@ const kbottomContainerColor = Color(0xFFEB1555);
 const klabelTextStyle = TextStyle(fontSize: 14.0, color: Color(0xFF8D8E98));
 
 const kNumberTextStyle = TextStyle(fontSize: 40.0, fontWeight: FontWeight.w900);
+
+const kLargeButtonTextStyle = TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kresultTextStyle = TextStyle(
+  color: Color(0xFF24D876),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBMITextStyle = TextStyle(fontSize: 100.0, fontWeight: FontWeight.bold);
+const kbodyTextStyle = TextStyle(fontSize: 22.0);
--- lib/screens/input_page.dart
+++ lib/screens/input_page.dart
@@ -1,10 +1,13 @@
-import 'package:bmi_calculator/result_page.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'icon_content.dart';
-import 'reusable_card.dart';
-import 'constants.dart';
+import 'package:bmi_calculator/components/icon_content.dart';
+import 'package:bmi_calculator/components/reusable_card.dart';
+import 'package:bmi_calculator/screens/result_page.dart';
+import 'package:bmi_calculator/components/bottom_button.dart';
+import 'package:bmi_calculator/components/round_icon_button.dart';
+import 'package:bmi_calculator/constants.dart';
 
 enum Gender {
   male,
@@ -196,43 +199,19 @@ class _InputPageState extends State<InputPage> {
                 ],
               ),
             ),
-            GestureDetector(
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context){
+            BottomButton(
+              buttonTitle: 'CALCULATE',
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
                   return ResultPage();
                 }));
               },
-              child: Container(
-                child: Text('CALCULATE'),
-                color: kbottomContainerColor,
-                margin: EdgeInsets.only(top: 10.0),
-                width: double.infinity,
-                height: BottomHeight,
-              ),
             ),
           ],
         ));
   }
 }
 
-class RoundIconButton extends StatelessWidget {
-  final IconData icon;
-  final Function input;
 
-  RoundIconButton({@required this.icon, @required this.input});
 
-  @override
-  Widget build(BuildContext context) {
-    return RawMaterialButton(
-      child: Icon(icon),
-      elevation: 6.0,
-      onPressed: input,
-      constraints: BoxConstraints.tightFor(
-        width: 56.0,
-        height: 56.0,
-      ),
-      shape: CircleBorder(),
-      fillColor: Color(0xFF4C4F5E),
-    );
-  }
-}
+
